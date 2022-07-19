12acf093dfb3c18f4cd2e33827c1b7cef6b44457
Antonio Mendoza Pérez <antonio.perez@temporal.io>
2022-07-19 19:21:38 +0200

    wip

--- src/main/java/examples/activity/failure/WfActivityFailImpl.java
+++ src/main/java/examples/activity/failure/WfActivityFailImpl.java
@@ -5,7 +5,6 @@ import io.temporal.activity.ActivityOptions;
 import io.temporal.common.RetryOptions;
 import io.temporal.failure.ActivityFailure;
 import io.temporal.workflow.Workflow;
-import org.slf4j.MDC;
 
 import java.time.Duration;
 
@@ -17,27 +16,21 @@ public class WfActivityFailImpl implements WfActivityFail {
             // every 30 seconds
             // which is too rare for the cancellations to be delivered in this example.
             .setHeartbeatTimeout(Duration.ofSeconds(5))
-                    .setRetryOptions(RetryOptions.newBuilder()
-                            .setInitialInterval(Duration.ofSeconds(1))
-                            .setMaximumAttempts(2)
-                            .build())
+            .setRetryOptions(RetryOptions.newBuilder()
+                    .setInitialInterval(Duration.ofSeconds(1))
+                    .setMaximumAttempts(2)
+                    .build())
             .setStartToCloseTimeout(Duration.ofSeconds(100))
             //Wait to cancel the activity, for cleaning purpose
             .setCancellationType(ActivityCancellationType.WAIT_CANCELLATION_COMPLETED)
             .build());
 
 
-
     @Override
     public String start() {
-
-            MDC.put("key","value");
-        try{
-
-
-
+        try {
             activity.longRunningMethod("");
-        }catch (ActivityFailure activityFailure){
+        } catch (ActivityFailure activityFailure) {
             // look inside activityFailure
             activity.compensateLongRunningMethod("");
         }
